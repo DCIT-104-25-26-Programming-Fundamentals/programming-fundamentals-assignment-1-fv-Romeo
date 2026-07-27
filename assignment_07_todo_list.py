6a32d74aa691fabf967f9d2d492fe882a650ab01
unknown <vuvlafafanyo@gmail.com>
2026-07-27 19:25:01 +0000

    Added the while loop to keep the app running ,view tasks, display menu and delete tasks

--- assignment_07_todo_list.py
+++ assignment_07_todo_list.py
@@ -91,3 +91,65 @@ def add_task(tasks):
     tasks.append(task)
 
     print(f'Task added: "{task}"')
+
+
+def view_tasks(tasks):
+
+    if len(tasks) == 0:
+        print("Your task list is empty.")
+
+    else:
+        print("Your Tasks:")
+
+        for i in range(len(tasks)):
+            print(f"{i + 1}. {tasks[i]}")
+
+def delete_task(tasks):
+
+    if len(tasks) == 0:
+        print("Your task list is empty.")
+
+    else:
+        view_tasks(tasks)
+
+        task_number = int(input("Enter task number to delete: "))
+
+        if task_number <= 0 or task_number > len(tasks):
+            print("Error invalid task number")
+
+        else:
+            removed_task = tasks.pop(task_number - 1)
+
+            print(f'Task "{removed_task}" has been removed.')
+
+def display_menu():
+
+    print("============================")
+    print("     TO-DO LIST MENU")
+    print("============================")
+    print("1. Add task")
+    print("2. View tasks")
+    print("3. Delete task")
+    print("4. Quit")
+
+while True:
+
+    display_menu()
+
+    choice = input("Enter your choice (1-4): ")
+
+    if choice == "1":
+        add_task(tasks)
+
+    elif choice == "2":
+        view_tasks(tasks)
+
+    elif choice == "3":
+        delete_task(tasks)
+
+    elif choice == "4":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Error invalid choice")                           
